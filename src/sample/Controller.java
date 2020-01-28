549d4d051789720c6080cc0376b44b36d2f2573c
PeterGanoczi <peter.ganoczi@akademiasovy.sk>
2020-01-28 12:09:56 +0100

    class city added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
@@ -11,13 +12,14 @@ import java.util.List;
 public class Controller {
 
     public ComboBox<String> boxCountry;
-    public ComboBox boxCity;
+    public ComboBox<String> boxCity;
     public Label lblPopulation;
     public Label lblTemperature;
     public Label lblHumidity;
     public Label lblDistance;
+    public Button btnOk;
     List countries;
-    List cities;
+    List<City> city;
     Database db=new Database();
 
     public Controller() throws Exception {
@@ -25,24 +27,44 @@ public class Controller {
         db.getCountries();
     }
 
-    public void chooseCountry(Event event) throws Exception {
-        countries=db.getCountries();
-        boxCountry.getItems().setAll(countries);
+    public void initialize(){
+        boxCity.setDisable(true);
+        btnOk.setDisable(true);
 
     }
 
+    public void chooseCountry(Event event) throws Exception {
+
+        countries=db.getCountries();
+        boxCountry.getItems().setAll(countries);
+        boxCity.setDisable(false);
+
+    }
 
     public void chooseCity(Event event) throws Exception {
-        cities=db.getCities(boxCountry.getValue());
-        boxCity.getItems().setAll(cities);
+
+
+        city=db.getCities(boxCountry.getValue());
+        boxCity.getItems().clear();
+        for (City s: city){
+            boxCity.getItems().add(s.getName());
+        }
+        btnOk.setDisable(false);
 
     }
 
     public void getCityInfo(ActionEvent event) throws Exception {
 
-        String pop=db.getPopulation((String) boxCity.getValue());
-
-        lblPopulation.setText(pop);
+        //String pop=db.getPopulation(boxCity.getValue(),boxCountry.getValue());
+        String population=boxCity.getValue();
+        for (City s:city){
+            if (s.getName().equalsIgnoreCase(population)){
+                lblPopulation.setText(String.valueOf(s.getPopulation()));
+            }
+        }
+        if (population==null){
+            return;
+        }
 
     }
 }
--- src/sample/Database.java
+++ src/sample/Database.java
@@ -9,6 +9,12 @@ import java.util.List;
 
 public class Database {
 
+    public static final String COUNTRIES="SELECT country.name FROM country ORDER BY name ASC";
+    public static final  String CITIES="SELECT city.name, country.Code2, city.CountryCode, JSON_EXTRACT(info, '$.Population') AS Info, country.name " +
+            "FROM country JOIN city ON country.code=city.countrycode " +
+            "WHERE country.name LIKE ? ORDER BY city.name ASC";
+
+
     private final String JDBC="com.mysql.cj.jdbc.Driver";
     private final String URL="jdbc:mysql://itsovy.sk:3306/world_x?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private Connection connection;
@@ -21,8 +27,8 @@ public class Database {
     }
 
     public List getCountries() throws Exception {
-        String countries="SELECT country.name FROM country";
-        PreparedStatement stmt=getConnection().prepareStatement(countries);
+
+        PreparedStatement stmt=getConnection().prepareStatement(COUNTRIES);
         ResultSet rs=stmt.executeQuery();
 
         List<String> list=new ArrayList<>();
@@ -35,23 +41,30 @@ public class Database {
     }
 
     public List getCities(String country) throws Exception{
-        String cities="SELECT city.name FROM country JOIN city ON country.code=city.countrycode WHERE country.name LIKE ?";
-        PreparedStatement stmt=getConnection().prepareStatement(cities);
+         PreparedStatement stmt=getConnection().prepareStatement(CITIES);
         stmt.setString(1,country);
         ResultSet rs=stmt.executeQuery();
 
-        List<String> list=new ArrayList<>();
+        List<City> list=new ArrayList<>();
         while (rs.next()){
-            list.add(rs.getString("Name"));
+            String name=rs.getString("city.name");
+            String code2=rs.getString("country.Code2");
+            String code3=rs.getString("city.CountryCode");
+            int population=rs.getInt("Info");
+            String countryName=rs.getString("country.name");
+            City newCity=new City(name, population,code3,code2,countryName);
+
+            list.add(newCity);
         }
         return list;
     }
 
-    public String getPopulation(String city) throws Exception {
+    public String getPopulation(String city, String country) throws Exception {
         String cityPop="";
-        String population="SELECT json_extract(info, '$.Population') FROM city WHERE name LIKE ?";
+        String population="SELECT json_extract(info, '$.Population') FROM city JOIN country ON country.code=city.countryCode WHERE city.name LIKE ? AND country.name LIKE ? ";
         PreparedStatement stmt=getConnection().prepareStatement(population);
         stmt.setString(1,city);
+        stmt.setString(2,country);
         ResultSet rs=stmt.executeQuery();
 
         if (rs.next()){
